087b9512c2b5ae3b5896b5a303f3869be14f59f1
Md Shakir Khalifa <shakir@macbookpro.mynetworksettings.com>
2025-12-16 11:46:32 -0500

    added code new

--- src/main/java/pages/GoogleHomePage.java
+++ src/main/java/pages/GoogleHomePage.java
@@ -4,4 +4,10 @@ public class GoogleHomePage {
     String titleName = "Google";
     int x;
     int y;
+
+    public void GoogleSearchPage() {
+        String titleName = "Google Search";
+        int a;
+        int b;
+    }
 }
