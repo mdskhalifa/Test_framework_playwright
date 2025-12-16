e4f9da9f7e73513d32f88f893c1dccf4bf79df3c
Md Shakir Khalifa <shakir@macbookpro.mynetworksettings.com>
2025-12-16 11:48:49 -0500

    updated class

--- src/main/java/pages/GoogleHomePage.java
+++ src/main/java/pages/GoogleHomePage.java
@@ -5,9 +5,13 @@ public class GoogleHomePage {
     int x;
     int y;
 
+    String url = "https://www.google.com";
+    String searchBox = "input[name='q']";
+
     public void GoogleSearchPage() {
         String titleName = "Google Search";
         int a;
         int b;
     }
 }
+
